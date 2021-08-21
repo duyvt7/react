bb3469ddfd4e999d6a8c4b150c5238fe554ec8d9
duyvu <vuthanhduy93@gmail.com>
2021-08-21 10:19:50 +0700

    class declaration

---
+++ class.js
@@ -0,0 +1,12 @@
+let rectangle = class {
+    constructor(weight){
+        this.weight = weight
+    }
+}
+console.log(rectangle.name)
+
+let Rectangle = class rectangle2 {
+    constructor(weight){
+        this.weight = weight
+    }
+}
