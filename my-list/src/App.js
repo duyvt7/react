7a623b0e80baaa42e1b25627d30c88a0b4457850
duyvu <vuthanhduy93@gmail.com>
2021-08-23 15:25:07 +0700

    react components

--- my-list/src/App.js
+++ my-list/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import toDoList from './components/toDoList';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <toDoList />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
---
+++ my-list/src/components/toDoList.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+class toDoList extends Component {
+    render() {
+        return (
+            <div className='toDoList'>
+                <p>a component example</p>
+            </div>
+        )
+    }
+}
+export default toDoList
