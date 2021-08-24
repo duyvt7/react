e2a8be3e1ec088e260a1033ed5c52387850dc5cd
duyvu <vuthanhduy93@gmail.com>
2021-08-24 09:22:51 +0700

    fix component fault

---
+++ test/src/component/toDoItem.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class TodoItem extends Component{
+    render(){
+        return (
+            <div className='TodoItem'>
+                <p>A component example.</p>
+            </div>
+        )}
+}
+export default TodoItem;
