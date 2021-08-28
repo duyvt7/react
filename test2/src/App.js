29ad4caafb404aa0f41357e782baddabeb8afb69
duyvu <vuthanhduy93@gmail.com>
2021-08-28 08:58:09 +0700

    add a condition

--- test2/src/App.js
+++ test2/src/App.js
@@ -7,19 +7,28 @@ class App extends Component{
   constructor(){
     super();
     this.ItemList= [
-      { title:'go shopping', isComplete:true},
-      { title:'go to school'},
-      { title:'go to the supermarket'},
+      // { title:'go shopping', isComplete:true},
+      // { title:'go to school'},
+      // { title:'go to the supermarket'},
     ]
   }
   render(){
-    return (
-      <div className='App' >
-        {this.ItemList.map((item, index) => 
-          <TodoItem key={index} item={item}/>
-        )}
-      </div>
-    )
+    if(this.ItemList.length > 0){
+      return (
+        <div className='App' >
+          {this.ItemList.map((item, index) => 
+            <TodoItem key={index} item={item}/>
+          )}
+        </div>
+      )
+    }else{
+      return(
+        <div className='App'>
+          <p>Nothing Here!</p>
+        </div>
+      )
+    }
+
   
   }
 }
